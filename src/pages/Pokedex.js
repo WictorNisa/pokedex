46b053d5e27d4d155505905a8b241aa543e9c00b
WictorNisa <wictornisa@hotmail.com>
2024-11-21 18:29:50 +0100

    Fixed search functionality

--- src/pages/Pokedex.js
+++ src/pages/Pokedex.js
@@ -2,18 +2,25 @@ import React, { useState, useEffect } from "react";
 import styles from "./Pokedex.module.css";
 import { getPokemonDetails, getPokemonList } from "../services/api";
 import PokemonCard from "../components/PokemonCard/PokemonCard";
+import SearchBar from "../components/SearchBar/SearchBar";
 
 const Pokedex = () => {
   const [pokemonList, setPokemonList] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [filteredPokemonList, setFilteredPokemonList] = useState([]);
+
+  const setFilteredPokemon = (filteredList) => {
+    setFilteredPokemonList(filteredList);
+  };
 
   useEffect(() => {
     const fetchPokemon = async () => {
       try {
         const data = await getPokemonList(20, 0);
-        console.log(data)
+        console.log(data);
         setPokemonList(data);
+        setFilteredPokemonList(data);
       } catch (error) {
         setError(error.message);
       } finally {
@@ -26,13 +33,26 @@ const Pokedex = () => {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error}</p>;
 
+  console.log("Filtered List in Pokedex:", filteredPokemonList);
+  console.log("Full List of Pokémon:", pokemonList);
   return (
     <section className={styles.pokedexContainer}>
       Pokedex Page\
-      Search for a pokemon..
+      <SearchBar
+        pokemonList={pokemonList}
+        setFilteredPokemon={setFilteredPokemon}
+      />
       <div className={styles.pokedexGalleryContainer}>
-        {pokemonList.map((pokemon) => (
-          <PokemonCard key={pokemon.name} src={pokemon.image} name={pokemon.name} types={pokemon.types}/>
+        {(filteredPokemonList.length > 0
+          ? filteredPokemonList
+          : pokemonList
+        ).map((pokemon) => (
+          <PokemonCard
+            key={pokemon.name}
+            src={pokemon.image}
+            name={pokemon.name}
+            types={pokemon.types}
+          />
         ))}
       </div>
     </section>
